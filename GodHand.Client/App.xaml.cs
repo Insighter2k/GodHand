a918695f6e77afd4a15e6970307c8b79628248a9
Insighter2k <noseriousadress@gmail.com>
2017-05-28 13:34:08 +0200

    Update

--- GodHand.Client/App.xaml.cs
+++ GodHand.Client/App.xaml.cs
@@ -15,8 +15,16 @@ namespace GodHand.Client
     {
         public App()
         {
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
             InitializeComponent();
 
+        }
+
+        void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            string errorMessage = string.Format("An unhandled exception occurred: {0}", e.Exception.Message);
+            MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
         }
     }
 }
--- Kakasi.Net/Kakasi.NET.TestWebApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kakasi.NET.TestWebApp.WebApiApplication" Language="C#" %>
